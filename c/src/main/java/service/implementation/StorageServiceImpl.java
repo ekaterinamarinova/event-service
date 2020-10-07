f2e9613957afc9ed6df50e819cdd0c15387d35ba
ekaterinamarinova <katherineeyall@gmail.com>
2020-10-07 17:54:35 +0300

    Added implementation in StorageServiceImpl.storeEventsInCSV

--- c/src/main/java/service/implementation/StorageServiceImpl.java
+++ c/src/main/java/service/implementation/StorageServiceImpl.java
@@ -2,11 +2,26 @@ package service.implementation;
 
 import service.definition.StorageService;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 public class StorageServiceImpl implements StorageService {
+
+    private static final String FILE_NAME = "Events.csv";
+    private static final String SEPARATOR = "/";
+
     @Override
     public void storeEventsInCSV(List<String> events) {
+        String pathToFile = SEPARATOR + "resources" + SEPARATOR + FILE_NAME;
 
+        try(FileWriter fileWriter = new FileWriter(new File(pathToFile))){
+            for (String e:events) {
+                fileWriter.write(e);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
