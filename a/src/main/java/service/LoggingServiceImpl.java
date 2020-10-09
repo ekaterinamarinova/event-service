f9525c06b13ac564fc3aed5ce578b6bae37fccab
ekaterinamarinova <katherineeyall@gmail.com>
2020-10-09 15:12:36 +0300

    Changed to work only with an event list; Added MonitoringServiceImpl dependency so the service can notify listeners when a log appears.

---
+++ a/src/main/java/service/LoggingServiceImpl.java
@@ -0,0 +1,24 @@
+package service;
+
+import definition.event.LoggingEvent;
+import definition.service.LoggingService;
+
+import java.util.List;
+
+public class LoggingServiceImpl implements LoggingService {
+
+    private final List<LoggingEvent> loggingEvents;
+    private final MonitoringServiceImpl monitoringService;
+
+    public LoggingServiceImpl(List<LoggingEvent> loggingEvents, MonitoringServiceImpl monitoringService) {
+        this.loggingEvents = loggingEvents;
+        this.monitoringService = monitoringService;
+    }
+
+    @Override
+    public synchronized void logEvent(LoggingEvent loggingEvent) {
+        loggingEvents.add(loggingEvent);
+        monitoringService.notifyMonitoringListeners();
+    }
+
+}
