581ff3a17efe1ce25d881140b5775b82513a8404
ekaterinamarinova <katherineeyall@gmail.com>
2020-10-16 00:55:50 +0300

    Using ServiceTracker instead of ServiceListener

--- b/src/main/java/activator/LoggingActivator.java
+++ b/src/main/java/activator/LoggingActivator.java
@@ -4,50 +4,33 @@ import definition.event.EventType;
 import definition.service.LoggingService;
 import implementation.event.LoggingEventImpl;
 import org.osgi.framework.*;
+import org.osgi.util.tracker.ServiceTracker;
 
 import java.time.LocalTime;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-public class LoggingActivator implements BundleActivator, ServiceListener {
+public class LoggingActivator implements BundleActivator {
 
     private BundleContext ctx;
-    private ServiceReference serviceReference;
+    private ServiceTracker<LoggingService, LoggingService> loggingServiceTracker;
 
     public void start(BundleContext ctx) {
         this.ctx = ctx;
-        try {
-            ctx.addServiceListener(this, "(objectclass=" + LoggingService.class.getName() + ")");
-        } catch (InvalidSyntaxException ise) {
-            ise.printStackTrace();
+        loggingServiceTracker = new ServiceTracker<>(ctx, LoggingService.class, null);
+        loggingServiceTracker.open();
+
+        if (!loggingServiceTracker.isEmpty()) {
+            ScheduledExecutorService scheduledExecutorService = new ScheduledThreadPoolExecutor(1);
+            scheduledExecutorService.scheduleAtFixedRate(() -> loggingServiceTracker.getService().logEvent(
+                    new LoggingEventImpl(EventType.Info, "Event Registered", LocalTime.now())
+            ), 0, 1000, TimeUnit.MILLISECONDS);
         }
     }
 
     public void stop(BundleContext bundleContext) {
-        if (serviceReference != null) {
-            ctx.ungetService(serviceReference);
-        }
+        this.loggingServiceTracker.close();
         this.ctx = null;
-    }
-
-    public void serviceChanged(ServiceEvent serviceEvent) {
-        int type = serviceEvent.getType();
-        switch (type) {
-            case (ServiceEvent.REGISTERED):
-                serviceReference = serviceEvent.getServiceReference();
-                LoggingService service = (LoggingService) (ctx.getService(serviceReference));
-                //start a thread and generate 100 events per second
-                ScheduledExecutorService scheduledExecutorService = new ScheduledThreadPoolExecutor(1);
-                scheduledExecutorService.scheduleAtFixedRate(() -> service.logEvent(new LoggingEventImpl(
-                        EventType.Info, "Event Registered", LocalTime.now()
-                )), 0, 1000, TimeUnit.MILLISECONDS);
-                break;
-            case (ServiceEvent.UNREGISTERING):
-                ctx.ungetService(serviceEvent.getServiceReference());
-                break;
-            default:
-                break;
-        }
     }
 }
--- c/src/main/java/activator/EventActivator.java
+++ c/src/main/java/activator/EventActivator.java
@@ -1,8 +1,9 @@
 package activator;
 
+import definition.event.EventType;
+import org.osgi.util.tracker.ServiceTracker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import definition.event.EventType;
 import definition.service.MonitoringService;
 import definition.service.RetrievingService;
 import org.osgi.framework.*;
@@ -13,69 +14,58 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-public class EventActivator implements BundleActivator, ServiceListener {
+public class EventActivator implements BundleActivator {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(EventActivator.class);
 
     private BundleContext ctx;
     private final StorageService storageService = new StorageService();
-    private ServiceReference<MonitoringService> monitoringServiceServiceReference;
-    private ServiceReference<RetrievingService> retrievingServiceServiceReference;
 
+    private ServiceTracker<RetrievingService, RetrievingService> retrievingServiceTracker;
+    private ServiceTracker<MonitoringService, MonitoringService> monitoringServiceTracker;
 
     public void start(BundleContext ctx) {
         this.ctx = ctx;
-        try {
-            ctx.addServiceListener(this, "(objectclass=" + MonitoringService.class.getName() + ")");
-            ctx.addServiceListener(this, "(objectclass=" + RetrievingService.class.getName() + ")");
-        } catch (InvalidSyntaxException ise) {
-            ise.printStackTrace();
+        startTrack(ctx);
+
+        if (!monitoringServiceTracker.isEmpty()) {
+            monitoringServiceTracker.getService().addMonitoringListener(event -> LOGGER.info(
+                    "Event received: " + event.getEventType() + " with message: " + event.getMessage()
+                    )
+            );
+        }
+
+        if (!retrievingServiceTracker.isEmpty()) {
+            ScheduledExecutorService scheduledExecutorService = new ScheduledThreadPoolExecutor(1);
+            scheduledExecutorService.scheduleAtFixedRate(() ->
+            {
+                try {
+                    storageService.storeEventsInCSV(retrievingServiceTracker.getService().retrieve(
+                            EventType.Info, LocalTime.now().minusSeconds(120), LocalTime.now()
+                    ));
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }, 0, 5, TimeUnit.MINUTES);
         }
     }
 
-    public void stop(BundleContext bundleContext) {
-        if (monitoringServiceServiceReference != null &&
-                retrievingServiceServiceReference != null) {
-            ctx.ungetService(monitoringServiceServiceReference);
-            ctx.ungetService(retrievingServiceServiceReference);
-        }
+    public void stop(BundleContext ctx) {
+        stopTrack();
         this.ctx = null;
     }
 
-    public void serviceChanged(ServiceEvent serviceEvent) {
-        int type = serviceEvent.getType();
-        switch (type) {
-            case (ServiceEvent.REGISTERED):
-                monitoringServiceServiceReference = (ServiceReference<MonitoringService>) serviceEvent.getServiceReference();
-                retrievingServiceServiceReference = (ServiceReference<RetrievingService>) serviceEvent.getServiceReference();
+    private void startTrack(BundleContext ctx) {
+        monitoringServiceTracker = new ServiceTracker<>(ctx, MonitoringService.class, null);
+        retrievingServiceTracker = new ServiceTracker<>(ctx, RetrievingService.class, null);
 
-                MonitoringService monitoringService = ctx.getService(monitoringServiceServiceReference);
-                monitoringService.addMonitoringListener(event -> LOGGER.debug(
-                        "Event received: " + event.getEventType() + " with message: " + event.getMessage()
-                        )
-                );
+        monitoringServiceTracker.open();
+        retrievingServiceTracker.open();
+    }
 
-                RetrievingService retrievingService = ctx.getService(retrievingServiceServiceReference);
-
-                ScheduledExecutorService scheduledExecutorService = new ScheduledThreadPoolExecutor(1);
-                scheduledExecutorService.scheduleAtFixedRate(() ->
-                {
-                    try {
-                        storageService.storeEventsInCSV(retrievingService.retrieve(
-                                EventType.Info, LocalTime.now().minusSeconds(120), LocalTime.now()
-                        ));
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    }
-                }, 0, 5, TimeUnit.MINUTES);
-
-                break;
-            case (ServiceEvent.UNREGISTERING):
-                ctx.ungetService(serviceEvent.getServiceReference());
-                break;
-            default:
-                break;
-        }
+    private void stopTrack() {
+        monitoringServiceTracker.close();
+        retrievingServiceTracker.close();
     }
 
 
